b2d4a113388a2740cfe6bbe7c2d3e6d83c468814
haxer-max <atharvashrawge@gmail.com>
2020-10-03 17:05:26 +0530

    converted stupid functions into class and socket working yay

--- src/components/Game/game.js
+++ src/components/Game/game.js
@@ -1,71 +1,147 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./../../App.css";
 import Box from "./Board/box.js";
 import io from "socket.io-client";
-/*
-class Game extends React.Component{
-    size = 9;
-    state={
-    BoardState:[
-            [1, 0, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 2, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ],
-    }
-    selected=-1;
-    useEffect(() => {
-        console.log(location.state)
-        socket = io("http://localhost:4000");
-        join(location.state.roomid)
-        socket.on("connect", () => {
-            console.log("connected");
-        });
-        socket.on('move',({i,j})=>{
-            console.log("board state is "+ i + " "+j)
-            console.log(selected);
-            //console.log(BoardState)
-            if(selected===-1){
-                if(BoardState[i][j]!==0){
-                    console.log(i*size+j );
-                    selected=i*size+j;
-                    //setSelected(i*size+j);
-                    //setSelected((selected) => [...selected, 1]);
-                    console.log(selected)
-                }
-            }
-            else{
-                
-                const aa = BoardState;
-                const aaa=aa[Math.floor(selected/size)][selected%size]
-                aa[Math.floor(selected/size)][selected%size]=0;
-                aa[Math.floor(i)][j]=aaa;
-                setBoardState(aa);
-                setSelected(-1);
-                selected=-1;
-                console.log(selected)
-            }
-        })
-
-        return () => {
-            socket.disconnect();
+import { render } from "@testing-library/react";
+const serverURI = "http://localhost:4000";
+const size = 9;
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            BoardState: [
+                [1, 0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 2, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ],
+            walls:[
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 2, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 3, 0, 0],
+                [0, 0, 0, 0, 2, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 4, 0, 0, 0, 0, 0, 2, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ]
         };
-    }, []);
-    function emmitmoved(i,j){
-        socket.emit('moved',{i,j})
+        this.selected = -1;
+
+        this.socket = io(serverURI);
     }
-    function join(roomid) {
+
+    componentDidMount() {
+        this.join(this.props.location.state.roomid);
+        this.socket.on("move", ({ i, j }) => {
+            console.log("board state is " + i + " " + j);
+            console.log(this.selected);
+            //console.log(BoardState)
+            if (this.selected === -1) {
+                if (this.state.BoardState[i][j] !== 0) {
+                    console.log(i * size + j);
+                    this.selected = i * size + j;
+                    console.log(this.selected);
+                }
+            } else {
+
+                const aa = this.state.BoardState;
+                const aaa =aa[Math.floor(this.selected / size)][this.selected % size];
+                aa[Math.floor(this.selected / size)][this.selected % size] = 0;
+                aa[Math.floor(i)][j] = aaa;
+                this.setState({
+                    BoardState: aa,
+                });
+
+                this.selected = -1;
+                console.log(this.selected);
+            }
+        });
+    }
+    emmitmoved(i, j) {
+        this.socket.emit("moved", { i, j });
+    }
+    join(roomid) {
         console.log("joining");
-        socket.emit("join", roomid );
+        this.socket.emit("join", roomid);
     }
 
+    movepiece(i, j) {
+        console.log("board state is " + i + " " + j);
+        console.log(this.selected);
+        //console.log(BoardState)
+        if (this.selected === -1) {
+            if (this.state.BoardState[i][j] !== 0) {
+                console.log(i * size + j);
+                this.selected = i * size + j;
+                console.log(this.selected);
+            }
+        } else {
+            const aa = this.state.BoardState;
+            console.log(aa);
+            const aaa =aa[Math.floor(this.selected / size)][this.selected % size];
+            aa[Math.floor(this.selected / size)][this.selected % size] = 0;
+            aa[Math.floor(i)][j] = aaa;
+            this.setState({
+                BoardState: aa,
+            });
+
+            this.selected = -1;
+            console.log(this.selected);
+        }
+    }
+    //setFunc((i,j)=>movepiece(i,j));
+
+    renbox(i, j) {
+
+        return (
+            <Box
+                key={j + i * size}
+                colour={j + i * size}
+                v={this.state.BoardState[i][j]}
+                w={this.state.walls[i][j]}
+                onClick={() => {
+                    this.emmitmoved(i, j);
+                }}
+            />
+        );
+    }
+    render() {
+        let ll = [];
+        for (var i = 0; i < size; i++) {
+            for (var j = 0; j < size; j++) {
+                //console.log('a')
+                ll.push(this.renbox(i, j));
+            }
+            ll.push(<span key={-i - 1}>&#010;</span>);
+        }
+        return (
+            <div>
+                <p>{this.props.location.state.roomid}</p>
+                <div>
+                    <h1>chess</h1>
+                    {ll}
+                </div>
+            </div>
+        );
+    }
+} 
 
 
+
+/*
+    const a=(data)=>{
+        setSelected(data)
+    }
+    const b=(data)=>{
+        setBoardState(data);
+    }
     function movepiece(i,j){
     
         console.log("board state is "+ i + " "+j)
@@ -74,25 +150,22 @@ class Game extends React.Component{
         if(selected===-1){
             if(BoardState[i][j]!==0){
                 console.log(i*size+j );
-                selected=i*size+j;
-                //setSelected((selected) => [...selected, 1]);
+                a(i*size+j)
+                //selected=i*size+j;
+                setSelected(i*size+j);
                 console.log(selected)
             }
         }
-        else{
-            
+        else{            
             const aa = BoardState;
             const aaa=aa[Math.floor(selected/size)][selected%size]
             aa[Math.floor(selected/size)][selected%size]=0;
             aa[Math.floor(i)][j]=aaa;
             setBoardState(aa);
-            //setSelected(-1);
+            setSelected(-1);
             console.log(selected)
         }
     }
-    //setFunc((i,j)=>movepiece(i,j));
-
-    
     function renbox(i,j){
        // console.log(i+ "h"+j)
         return (
@@ -100,11 +173,11 @@ class Game extends React.Component{
                 key={j+i*size}
                 colour={j+i*size}
                 v={BoardState[i][j]} 
-                onClick={()=>{emmitmoved(i,j)}}
+                w={walls[i][j]}
+                onClick={()=>{movepiece(i,j)}}
             />
         );
     }
-        //const ll=state.a.map((currentValue, index)=>renbox(index));
     let ll=[]
     for(var i=0;i<size;i++)
     {
@@ -129,9 +202,9 @@ class Game extends React.Component{
         </div>
     );
 }
-
-
 */
+
+/*
 let socket
 
 function Game({ location }) {
@@ -207,86 +280,4 @@ function Game({ location }) {
         console.log("joining");
         socket.emit("join", roomid );
     }
-*/
-    const a=(data)=>{
-        setSelected(data)
-    }
-    const b=(data)=>{
-        setBoardState(data);
-    }
-    function movepiece(i,j){
-    
-        console.log("board state is "+ i + " "+j)
-        console.log(selected);
-        //console.log(BoardState)
-        if(selected===-1){
-            if(BoardState[i][j]!==0){
-                console.log(i*size+j );
-                a(i*size+j)
-                //selected=i*size+j;
-                setSelected(i*size+j);
-                console.log(selected)
-            }
-        }
-        else{
-            
-            const aa = BoardState;
-            const aaa=aa[Math.floor(selected/size)][selected%size]
-            aa[Math.floor(selected/size)][selected%size]=0;
-            aa[Math.floor(i)][j]=aaa;
-            setBoardState(aa);
-            setSelected(-1);
-            console.log(selected)
-        }
-    }
-    //setFunc((i,j)=>movepiece(i,j));
-
-    
-    function renbox(i,j){
-       // console.log(i+ "h"+j)
-        return (
-            <Box 
-                key={j+i*size}
-                colour={j+i*size}
-                v={BoardState[i][j]} 
-                w={walls[i][j]}
-                onClick={()=>{movepiece(i,j)}}
-            />
-        );
-    }
-        //const ll=state.a.map((currentValue, index)=>renbox(index));
-    let ll=[]
-    for(var i=0;i<size;i++)
-    {
-        for(var j=0;j<size;j++)
-        {
-            //console.log('a')
-            ll.push(renbox(i,j))
-        }
-        ll.push(<span key={-i-1}>&#010;</span>)
-    }
-
-
-    return (
-        <div>
-            <p>{location.state.roomid}</p>
-            <div>
-                
-                <h1>chess</h1>
-                {ll}
-            </div>
-
-        </div>
-    );
-}
-
-export default Game;
-
-/*            <Board 
-                size={BoardSize} 
-                BoardState={BoardState} 
-                setBoardState={setBoardState} 
-                emmitmoved={emmitmoved} 
-                setFunc={setFunc}
-            />
-             */
+*/ export default Game;
