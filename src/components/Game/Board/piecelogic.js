82f9d1517f50893c1b818e6795d72a7ea47aa677
haxer-max <atharvashrawge@gmail.com>
2020-10-09 00:01:55 +0530

    seperated the file for logic

---
+++ src/components/Game/Board/piecelogic.js
@@ -0,0 +1,68 @@
+export default function piecelogic(boardtemp,selectedPiece,selectedboxI,selectedboxJ,i,j){
+    if(selectedPiece===1){
+        const aa=boardtemp;
+        console.log("working");
+        console.log(selectedboxI);
+        const first = [1,2,2,1,-1,-2,-2,-1];
+        const second = [2,1,-1,-2,-2,-1,1,2];
+        for(var k=0;k<8;k++){
+            if(first[k]+selectedboxI===i && second[k]+selectedboxJ===j){
+                boardtemp[i][j]=boardtemp[selectedboxI][selectedboxJ];
+                boardtemp[selectedboxI][selectedboxJ]=0;
+                console.log(boardtemp);
+                const aa=boardtemp;
+                break;
+            }
+        }
+        selectedPiece=-1;
+    }else if(selectedPiece===2){
+        const aa=boardtemp;
+        console.log("working");
+        console.log(selectedboxJ);
+        const first = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,-1,-2,-3,-4,-5,-6,-7,-8,-9,-1,-2,-3,-4,-5,-6,-7,-8,-9];
+        const second = [1,2,3,4,5,6,7,8,9,-1,-2,-3,-4,-5,-6,-7,-8,-9,1,2,3,4,5,6,7,8,9,-1,-2,-3,-4,-5,-6,-7,-8,-9,];
+        for(var k=0;k<36;k++){
+            if(first[k]+selectedboxI===i && second[k]+selectedboxJ===j){
+                boardtemp[i][j]=boardtemp[selectedboxI][selectedboxJ];
+                boardtemp[selectedboxI][selectedboxJ]=0;
+                console.log(boardtemp);
+                const aa=boardtemp;            
+                break;
+            }
+        }
+        selectedPiece=-1;
+    }else if(selectedPiece===3){
+        const aa=boardtemp;
+        console.log("working");
+        console.log(selectedboxI);
+        const first = [1,2,3,4,5,6,7,8,9,-1,-2,-3,-4,-5,-6,-7,-8,-9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        const second = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,-1,-2,-3,-4,-5,-6,-7,-8,-9];
+        for(var k=0;k<36;k++){
+            if(first[k]+selectedboxI===i && second[k]+selectedboxJ===j){
+                boardtemp[i][j]=boardtemp[selectedboxI][selectedboxJ];
+                boardtemp[selectedboxI][selectedboxJ]=0;
+                console.log(boardtemp);
+                const aa=boardtemp;             
+                break;
+            }
+        }
+        selectedPiece=-1;
+    }else if(selectedPiece===4){
+        const aa=boardtemp;
+        console.log("working");
+        console.log(selectedboxI);
+        const first = [0,1,1,1,0,-1,-1,-1];
+        const second = [1,1,0,1,-1,-1,0,1];
+        for(var k=0;k<8;k++){
+            if(first[k]+selectedboxI===i && second[k]+selectedboxJ===j){
+                boardtemp[i][j]=boardtemp[selectedboxI][selectedboxJ];
+                boardtemp[selectedboxI][selectedboxJ]=0;
+                console.log(boardtemp);
+                const aa=boardtemp;                
+                break;
+            }
+        }
+        
+    }
+    return boardtemp;
+}
--- src/components/Game/game.js
+++ src/components/Game/game.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./../../App.css";
 import Box from "./Board/box.js";
 import io from "socket.io-client";
+import piecelogic from './Board/piecelogic';
 //import { render } from "@testing-library/react";
 const serverURI = "http://localhost:4000";
 const size = 9;
@@ -39,132 +40,24 @@ class Game extends React.Component {
 
         this.movepiece=(i, j)=> {
             console.log("board state is " + i + " " + j);
-            //console.log(this.selected);
-            //const d=0;
-            //const e=0;
-            //console.log(BoardState)
-            if (this.selectedPiece === -1) {
+            if (this.selectedPiece === -1 ) {
                 if (this.state.BoardState[i][j] !== 0) {
-                    console.log(i * size + j);
                     this.selectedboxI=i;
                     this.selectedboxJ=j;
-
-                    //console.log(this.selected);
-                    if(this.state.BoardState[i][j]===1 || this.state.BoardState[i][j]===5){
-                        this.selectedPiece = 1;
-                    }
-                    if(this.state.BoardState[i][j]===4 || this.state.BoardState[i][j]===8){
-                        this.selectedPiece = 4;
-                    }
-                    if(this.state.BoardState[i][j]===2 || this.state.BoardState[i][j]===6){
-                        this.selectedPiece = 2;
-                    }
-                    if(this.state.BoardState[i][j]===3 || this.state.BoardState[i][j]===7){
-                        this.selectedPiece = 3;
-                    }
-                    /* var d=i;
-                    var e=j; */
-                    console.log(this.selectedPiece);
-                    console.log(this.selectedboxI);
-                    console.log(this.selectedboxJ);
+                    if(this.state.BoardState[i][j]===1 || this.state.BoardState[i][j]===5) this.selectedPiece = 1;
+                    else if(this.state.BoardState[i][j]===4 || this.state.BoardState[i][j]===8) this.selectedPiece = 4;
+                    else if(this.state.BoardState[i][j]===2 || this.state.BoardState[i][j]===6) this.selectedPiece = 2;
+                    else if(this.state.BoardState[i][j]===3 || this.state.BoardState[i][j]===7) this.selectedPiece = 3;
                 }
             }else{
-                if(this.selectedPiece===1){
-                const aa=this.state.BoardState;
-                console.log("working");
-                console.log(this.selectedboxI);
-                const first = [1,2,2,1,-1,-2,-2,-1];
-                const second = [2,1,-1,-2,-2,-1,1,2];
-                for(var k=0;k<8;k++){
-                    if(first[k]+this.selectedboxI===i && second[k]+this.selectedboxJ===j){
-                        this.state.BoardState[i][j]=this.state.BoardState[this.selectedboxI][this.selectedboxJ];
-                        this.state.BoardState[this.selectedboxI][this.selectedboxJ]=0;
-                        console.log(this.state.BoardState);
-                        const aa=this.state.BoardState;
-                        this.setState({
-                            BoardState: aa,
-                        });
-                        
-                        break;
-                    }
-                }
-                this.selectedPiece=-1;
-            }else if(this.selectedPiece===2){
-                const aa=this.state.BoardState;
-                console.log("working");
-                console.log(this.selectedboxJ);
-                const first = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,-1,-2,-3,-4,-5,-6,-7,-8,-9,-1,-2,-3,-4,-5,-6,-7,-8,-9];
-                const second = [1,2,3,4,5,6,7,8,9,-1,-2,-3,-4,-5,-6,-7,-8,-9,1,2,3,4,5,6,7,8,9,-1,-2,-3,-4,-5,-6,-7,-8,-9,];
-                for(var k=0;k<36;k++){
-                    if(first[k]+this.selectedboxI===i && second[k]+this.selectedboxJ===j){
-                        this.state.BoardState[i][j]=this.state.BoardState[this.selectedboxI][this.selectedboxJ];
-                        this.state.BoardState[this.selectedboxI][this.selectedboxJ]=0;
-                        console.log(this.state.BoardState);
-                        const aa=this.state.BoardState;
-                        this.setState({
-                            BoardState: aa,
-                        });
-                        
-                        break;
-                    }
-                }
-                this.selectedPiece=-1;
-            }else if(this.selectedPiece===3){
-                const aa=this.state.BoardState;
-                console.log("working");
-                console.log(this.selectedboxI);
-                const first = [1,2,3,4,5,6,7,8,9,-1,-2,-3,-4,-5,-6,-7,-8,-9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-                const second = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,-1,-2,-3,-4,-5,-6,-7,-8,-9];
-                for(var k=0;k<36;k++){
-                    if(first[k]+this.selectedboxI===i && second[k]+this.selectedboxJ===j){
-                        this.state.BoardState[i][j]=this.state.BoardState[this.selectedboxI][this.selectedboxJ];
-                        this.state.BoardState[this.selectedboxI][this.selectedboxJ]=0;
-                        console.log(this.state.BoardState);
-                        const aa=this.state.BoardState;
-                        this.setState({
-                            BoardState: aa,
-                        });
-                        
-                        break;
-                    }
-                }
-                this.selectedPiece=-1;
-            }else if(this.selectedPiece===4){
-                const aa=this.state.BoardState;
-                console.log("working");
-                console.log(this.selectedboxI);
-                const first = [0,1,1,1,0,-1,-1,-1];
-                const second = [1,1,0,1,-1,-1,0,1];
-                for(var k=0;k<8;k++){
-                    if(first[k]+this.selectedboxI===i && second[k]+this.selectedboxJ===j){
-                        this.state.BoardState[i][j]=this.state.BoardState[this.selectedboxI][this.selectedboxJ];
-                        this.state.BoardState[this.selectedboxI][this.selectedboxJ]=0;
-                        console.log(this.state.BoardState);
-                        const aa=this.state.BoardState;
-                        this.setState({
-                            BoardState: aa,
-                        });
-                        
-                        break;
-                    }
-                }
+                let boardtemp= this.state.BoardState.map(function(arr) {
+                    return arr.slice();
+                });
+                this.setState({
+                    BoardState:piecelogic(boardtemp,this.selectedPiece,this.selectedboxI,this.selectedboxJ,i,j)
+                })
                 this.selectedPiece=-1;
             }
-        }
-            /*  else {
-                const aa = this.state.BoardState;
-                console.log(aa);
-                const aaa =aa[Math.floor(this.selected / size)][this.selected % size];
-                this.state.BoardState[Math.floor(this.selected / size)][this.selected % size]
-                aa[Math.floor(this.selected / size)][this.selected % size] = 0;
-                aa[Math.floor(i)][j] = aaa;
-                this.setState({
-                    BoardState: aa,
-                });
-    
-                this.selected = -1;
-                console.log(this.selected);
-            } */
         };
     }
 
