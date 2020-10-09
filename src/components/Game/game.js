1ea3a9eb3e634b53ecb36ff979db48913ff4a073
haxer-max <atharvashrawge@gmail.com>
2020-10-09 18:20:38 +0530

    board expanded and button added

--- src/components/Game/game.js
+++ src/components/Game/game.js
@@ -5,32 +5,35 @@ import io from "socket.io-client";
 import piecelogic from './Board/piecelogic';
 //import { render } from "@testing-library/react";
 const serverURI = "http://localhost:4000";
-const size = 9;
+const sizex = 15;
+const sizey = 10;
 class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             BoardState: [
-                [1, 0, 2, 0, 3, 0, 1, 0, 4],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [5, 0, 6, 0, 7, 0, 5, 0, 8],
+                [1, 0, 2, 0, 3, 0, 1, 0, 4, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [5, 0, 6, 0, 7, 0, 5, 0, 8, 0, 0, 0, 0, 0, 0],
             ],
             walls:[
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 2, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 3, 0, 0],
-                [0, 0, 0, 0, 2, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 4, 0, 0, 0, 0, 0, 2, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 4, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             ]
         };
         this.selectedboxI=-1;
@@ -85,8 +88,8 @@ class Game extends React.Component {
 
         return (
             <Box
-                key={j + i * size}
-                colour={j + i * size}
+                key={j + i * sizex}
+                colour={j + i * sizex}
                 v={this.state.BoardState[i][j]}
                 w={this.state.walls[i][j]}
                 onClick={() => {
@@ -97,8 +100,8 @@ class Game extends React.Component {
     }
     render() {
         let ll = [];
-        for (var i = 0; i < size; i++) {
-            for (var j = 0; j < size; j++) {
+        for (var i = 0; i < sizey; i++) {
+            for (var j = 0; j < sizex; j++) {
                 //console.log('a')
                 ll.push(this.renbox(i, j));
             }
@@ -109,7 +112,20 @@ class Game extends React.Component {
                 <p>{this.props.location.state.roomid}</p>
                 <div>
                     <h1>chess</h1>
-                    {ll}
+                    <div style={{display:"flex"}}>
+                        <button className="rotatebutton" > rotate </button>
+                        <button className="rotatebutton" > rotate </button>
+                        <button className="rotatebutton" > rotate </button>
+                    </div>
+                    <div>
+                        {ll}
+                    </div>
+                    <div style={{display:"flex"}}>
+                        <button className="rotatebutton" > rotate </button>
+                        <button className="rotatebutton" > rotate </button>
+                        <button className="rotatebutton" > rotate </button>
+                    </div>
+                    <button>rotate</button><button>rotate</button><button>rotate</button>
                 </div>
             </div>
         );
